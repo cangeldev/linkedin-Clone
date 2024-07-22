4f4c3468b7a693ea47202bfa043e5dce7d510808
CAN GEL <can.gel@hotmail.com>
2024-07-22 23:28:23 +0300

    Vektor Icons library  & label style added.

--- src/assets/colors/colors.ts
+++ src/assets/colors/colors.ts
@@ -2,5 +2,6 @@ export default {
     black: "#000",
     white: "#fff",
     red: "#FF0000",
-    orange: "#f99935"
+    orange: "#f99935",
+    grey: "#808080"
 }
--- src/navigation/tabNavigation.tsx
+++ src/navigation/tabNavigation.tsx
@@ -1,38 +1,81 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { HomeScreen, JobsScreen, MyNetworkScreen, NotificationScreen, PostScreen } from 'screens/bottomTabScreens'
+import Icon from 'react-native-vector-icons/FontAwesome6'
+import IconF from 'react-native-vector-icons/MaterialCommunityIcons'
+import IconI from 'react-native-vector-icons/Octicons'
+import colors from 'assets/colors/colors'
 
-export const TabNavigation = () => {
+const Tab = createBottomTabNavigator()
 
-    const Tab = createBottomTabNavigator()
+const getTabBarIcon = (name: any, size = 19, IconComponent = Icon) => ({ color }: any) => (
+    <IconComponent name={name} color={color} style={{ fontSize: size }} />
+)
 
-    return (
-        <Tab.Navigator screenOptions={{ headerShown: false,}}>
+
+const screens = [
+    {
+        name: "HomeScreen",
+        component: HomeScreen,
+        options: {
+            title: "Ana Sayfa",
+            tabBarIcon: getTabBarIcon("house-chimney")
+        }
+    },
+    {
+        name: "MyNetworkScreen",
+        component: MyNetworkScreen,
+        options: {
+            title: "Ağım",
+            tabBarIcon: getTabBarIcon("user-group")
+        }
+    },
+    {
+        name: "PostScreen",
+        component: PostScreen,
+        options: {
+            title: "Yayınla",
+            tabBarIcon: getTabBarIcon("plus-box", 24, IconF)
+        }
+    },
+    {
+        name: "NotificationScreen",
+        component: NotificationScreen,
+        options: {
+            title: "Bildirimler",
+            tabBarIcon: getTabBarIcon("bell-fill", 19, IconI)
+        }
+    },
+    {
+        name: "JobsScreen",
+        component: JobsScreen,
+        options: {
+            title: "İş ilanları",
+            tabBarIcon: getTabBarIcon("briefcase")
+        }
+    }
+]
+
+export const TabNavigation = () => (
+    <Tab.Navigator
+        screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: colors.black,
+            tabBarInactiveTintColor: colors.grey,
+            tabBarLabelStyle: {
+                marginTop: -10,
+                marginBottom: 4,
+                fontWeight: "500"
+            }
+        }}
+    >
+        {screens.map(screen => (
             <Tab.Screen
-                name="HomeScreen"
-                component={HomeScreen}
-                options={{title:"Home"}}
+                key={screen.name}
+                name={screen.name}
+                component={screen.component}
+                options={screen.options}
             />
-            <Tab.Screen
-                name="MyNetworkScreen"
-                component={MyNetworkScreen}
-                options={{title:"Ağım"}}
-            />
-            <Tab.Screen
-                name="PostScreen"
-                component={PostScreen}
-                options={{title:"Yayınla"}}
-            />
-            <Tab.Screen
-                name="NotificationScreen"
-                component={NotificationScreen}
-                options={{title:"Bildirimler"}}
-            />
-            <Tab.Screen
-                name="JobsScreen"
-                component={JobsScreen}
-                options={{title:"İş ilanları"}}
-            />
-        </Tab.Navigator>
-    )
-}
+        ))}
+    </Tab.Navigator>
+)
