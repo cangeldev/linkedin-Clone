d058e97e1c0b2a903c9d2e8b8e72814566033982
CAN GEL <can.gel@hotmail.com>
2024-07-23 13:57:20 +0300

    Drawer menu pages added.

---
+++ src/screens/pages/groupsPage/groupsPage.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const GroupsPage = () => {
+    return (
+        <View>
+            <Text>groupsPage</Text>
+        </View>
+    )
+}
---
+++ src/screens/pages/savedPostsPage/savedPostsPage.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const SavedPostsPage = () => {
+    return (
+        <View>
+            <Text>savedPostsPage</Text>
+        </View>
+    )
+}
