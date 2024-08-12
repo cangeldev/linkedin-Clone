5e6ea97650100aa15d4109ac787046758586e7eb
CAN GEL <can.gel@hotmail.com>
2024-08-12 20:21:05 +0300

    Modal footer section added to post screen.

--- src/screens/bottomTabScreens/post/postModal.tsx
+++ src/screens/bottomTabScreens/post/postModal.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import { Modal, View, Text, Image, TouchableOpacity } from 'react-native'
-import style from './style'
+import React, { useState } from 'react'
+import { Modal, View, Text, Image, TouchableOpacity, TextInput } from 'react-native'
+import styles from './style'
 import IconA from 'react-native-vector-icons/AntDesign'
 import IconO from 'react-native-vector-icons/Octicons'
+import IconI from 'react-native-vector-icons/Ionicons'
+import IconF from 'react-native-vector-icons/Fontisto'
+import IconF5 from 'react-native-vector-icons/FontAwesome6'
 import { profileExample } from 'assets'
+import colors from 'assets/colors/colors'
 
 interface PostModalProps {
     isVisible: boolean
@@ -11,34 +15,49 @@ interface PostModalProps {
 }
 
 const PostModal: React.FC<PostModalProps> = ({ isVisible, onClose }) => {
+    const [postContent, setPostContent] = useState<string>('')
+    const isPostButtonDisabled = postContent.trim().length === 0
+
     return (
-        <Modal
-            visible={isVisible}
-            transparent={true}
-            animationType="slide"
-            onRequestClose={onClose}
-        >
-            <View style={style.modalContainer}>
-                <View style={style.modalHeader}>
-                    <IconA name="close" style={style.closeIcon} onPress={onClose} />
-                    <Image source={profileExample} style={style.userProfileImage} />
-                    <Text style={style.headerTitle}>
+        <Modal visible={isVisible} animationType="slide" onRequestClose={onClose}>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <IconA name="close" style={styles.closeIcon} onPress={onClose} />
+                    <Image source={profileExample} style={styles.profileImage} />
+                    <Text style={styles.username}>
                         Herhangi biri
-                        {""} <IconA name="caretdown" onPress={onClose} style={style.dropdownIcon} />
+                        <IconA name="caretdown" style={styles.dropdownIcon} />
                     </Text>
-                    <View style={{ flexDirection: "row", alignItems: "center" }}>
-                        <IconO name="clock" style={style.timeIcon} onPress={onClose} />
-                        <TouchableOpacity>
-                            <Text style={style.postButtonText}>
+                    <View style={styles.innerHeader}>
+                        <IconO name="clock" style={styles.timeIcon} />
+                        <TouchableOpacity disabled={isPostButtonDisabled}>
+                            <Text style={isPostButtonDisabled ? styles.disabledPostButton : styles.enabledPostButton}>
                                 Postala
                             </Text>
                         </TouchableOpacity>
                     </View>
                 </View>
-                <Text>Post Something</Text>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        autoFocus
+                        multiline
+                        placeholderTextColor={colors.darkGrey}
+                        placeholder='Düşüncelerinizi paylaşın...'
+                        style={styles.textInput}
+                        onChangeText={setPostContent}
+                        value={postContent}
+                    />
+                </View>
+                <View style={styles.footer}>
+                    <TouchableOpacity style={styles.aiButton}>
+                        <IconI name="sparkles" style={styles.sparklesIcon} />
+                        <Text style={styles.aiText}>Yapay zeka ile yeniden yazın</Text>
+                    </TouchableOpacity>
+                    <IconF5 name="image" style={styles.footerIcon} />
+                    <IconF name="plus-a" style={styles.footerIcon} />
+                </View>
             </View>
         </Modal>
     )
 }
-
 export default React.memo(PostModal)
--- src/screens/bottomTabScreens/post/style.ts
+++ src/screens/bottomTabScreens/post/style.ts
@@ -1,51 +1,97 @@
-import colors from "assets/colors/colors"
-import { StyleSheet } from "react-native"
+import { StyleSheet } from 'react-native'
+import colors from 'assets/colors/colors'
 
 export default StyleSheet.create({
-    modalContainer: {
+    container: {
         flex: 1,
         backgroundColor: colors.white,
         paddingHorizontal: 13
     },
-    modalHeader: {
+    header: {
         flexDirection: "row",
         alignItems: "center",
-        paddingVertical: 7,
+        paddingVertical: 7
     },
     closeIcon: {
         fontSize: 25,
         color: colors.black,
         marginRight: 20
     },
-    userProfileImage: {
+    profileImage: {
         width: 30,
         height: 30,
         borderRadius: 15,
         marginRight: 10
     },
-    headerTitle: {
-        textAlignVertical: "center", 
-        fontSize: 16, 
-        fontWeight: "500", 
-        color: colors.darkGrey, 
-        flex: 1
+    username: {
+        flex: 1,
+        fontSize: 16,
+        fontWeight: "500",
+        color: colors.darkGrey,
+        textAlignVertical: "center"
     },
     dropdownIcon: {
-        fontSize: 12, 
-        color: colors.black 
+        fontSize: 12,
+        color: colors.black,
     },
-    timeIcon: { 
-        fontSize: 19, 
-        marginRight: 10, 
-        color: colors.black 
+    innerHeader: {
+        flexDirection: "row",
+        alignItems: "center"
     },
-    postButtonText: { 
-        color: "#a3a3a3", 
-        paddingVertical: 5, 
-        paddingHorizontal: 12, 
-        backgroundColor: colors.lightGrey, 
-        borderRadius: 25, 
-        fontSize: 16, 
-        fontWeight: "500" 
+    timeIcon: {
+        fontSize: 19,
+        marginRight: 10,
+        color: colors.black
+    },
+    disabledPostButton: {
+        color: "#a3a3a3",
+        paddingVertical: 5,
+        paddingHorizontal: 12,
+        backgroundColor: colors.lightGrey,
+        borderRadius: 25,
+        fontSize: 16,
+        fontWeight: "500"
+    },
+    enabledPostButton: {
+        color: colors.white,
+        paddingVertical: 5,
+        paddingHorizontal: 12,
+        backgroundColor: "#0b66bf",
+        borderRadius: 25,
+        fontSize: 16,
+        fontWeight: "500"
+    },
+    inputContainer: {
+        flex: 1,
+    },
+    textInput: {
+        fontSize: 17,
+        color: colors.black
+    },
+    footer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 8
+    },
+    sparklesIcon: {
+        fontSize: 16,
+        color: "#c47c18",
+        marginRight: 5
+    },
+    aiButton: {
+        flexDirection: "row",
+        paddingHorizontal: 15,
+        paddingVertical: 7,
+        borderRadius: 25,
+        borderWidth: 1.5,
+        alignItems: "center"
+    },
+    aiText: {
+        fontSize: 16,
+        fontWeight: "500"
+    },
+    footerIcon: {
+        fontSize: 20
     }
 })
