4fb7ee9b9d3e7e8a0e4d59d41f0044af45e86f64
CAN GEL <can.gel@hotmail.com>
2024-09-19 14:01:25 +0300

    Adjusted to show Empty Notifications Card when there are no notifications.

--- src/screens/bottomTabScreens/notification/tabViewScreens/allNotifications/allNotifications.tsx
+++ src/screens/bottomTabScreens/notification/tabViewScreens/allNotifications/allNotifications.tsx
@@ -5,7 +5,6 @@ import style from './style'
 import { acceptFriendRequest, fetchUsersWithSenderInfo } from 'services/firebase/firebase'
 
 export const AllNotifications = () => {
-
     const [requests, setRequests] = useState<any[]>([])
 
     useEffect(() => {
@@ -21,18 +20,21 @@ export const AllNotifications = () => {
             <Image source={{ uri: item.senderInfo?.profileImageUrl }} style={{ width: 50, height: 50 }} />
             <Text>{item.senderInfo?.name}</Text>
             <Text>{item.id}</Text>
-            <Button title='kabul et' onPress={() => acceptFriendRequest(item.id)} />
+            <Button title='Kabul Et' onPress={() => acceptFriendRequest(item.id)} />
         </View>
     ), [])
 
     return (
         <View style={style.container}>
-            {/* <EmptyNotificationsCard
-                notification='Yeni bildirim yok'
-                notificationInfo='Ana sayfanızdaki diğer güncellemeleri görüntüleyin'
-                buttonTitle='Ana Sayfaya Git'
-            /> */}
-            <FlatList data={requests} renderItem={renderItem} />
+            {requests.length === 0 ? (
+                <EmptyNotificationsCard
+                    notification='Yeni bildirim yok'
+                    notificationInfo='Ana sayfanızdaki diğer güncellemeleri görüntüleyin'
+                    buttonTitle='Ana Sayfaya Git'
+                />
+            ) : (
+                <FlatList data={requests} renderItem={renderItem} keyExtractor={(item) => item.id} />
+            )}
         </View>
     )
 }
